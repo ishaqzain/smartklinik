5577703ea9b15f42eca0bef95cb0ec179e3f56dc
Muchamad Ishaq Z <ishaq.muchamad@gmail.com>
2023-11-11 21:17:43 +0700

    add: beranda & login

---
+++ lib/ui/login.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:smartklinik/ui/beranda.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameCtrl = TextEditingController();
+  final _passwordCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Login Admin", style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),),
+                const SizedBox(height: 50,),
+                Center(
+                  child: Form(
+                    key: _formKey,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        children: [
+                          // user
+                          _usernameTextField(),
+                          const SizedBox(height: 20,),
+                          // pass
+                          _passwordTextField(),
+                          const SizedBox(height: 40,),
+                          // tombol
+                          _tombolLogin()
+                        ],
+                      ),
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+  // user
+  Widget _usernameTextField(){
+    return TextFormField(
+    decoration: const InputDecoration(labelText: "Username"),
+    controller: _usernameCtrl,
+    );
+  }
+  // password
+  Widget _passwordTextField(){
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Password"),
+      controller: _passwordCtrl,
+      obscureText: true,
+    );
+  }
+  // button
+  Widget _tombolLogin(){
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        child: const Text("Login"),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(
+            builder:(context) => const Beranda()
+          ));
+        },
+      ),
+    );
+  }
+}
