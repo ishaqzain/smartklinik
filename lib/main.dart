77b536b7fd7be35336ef6c34727ed6a6d60fa518
Muchamad Ishaq Z <ishaq.muchamad@gmail.com>
2023-12-10 11:13:24 +0700

    add: pasien completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smartklinik/ui/beranda.dart';
+import 'package:smartklinik/ui/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: "Smart Klinik",
       debugShowCheckedModeBanner: false,
-      home: Beranda(),
+      home: Login(),
     );
   }
 }
