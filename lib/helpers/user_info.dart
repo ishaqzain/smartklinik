4c1917fc2fa65694fb282af215c8801f68ad5646
Muchamad Ishaq Z <ishaq.muchamad@gmail.com>
2024-01-12 21:35:37 +0700

    add: login by server API

--- lib/helpers/user_info.dart
+++ lib/helpers/user_info.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 const String token ='token';
 const String userId ='userID';
 const String username = 'username';
+const String email = 'email';
 class UserInfo{
   Future setToken(String value) async{
     final SharedPreferences pref = await SharedPreferences.getInstance();
@@ -31,6 +32,16 @@ class UserInfo{
   Future<String?> getUserName() async {
     final SharedPreferences pref = await SharedPreferences.getInstance();
     return pref.getString(username).toString();
+  }
+
+  Future setEmail(String value) async{
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.setString(email, value);
+  }
+
+  Future<String?> getEmail() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getString(email).toString();
   }
 
   Future<void> logout() async{
--- lib/service/login_service.dart
+++ lib/service/login_service.dart
@@ -1,14 +1,29 @@
 import 'package:smartklinik/helpers/user_info.dart';
+import 'package:dio/dio.dart';
+import 'package:smartklinik/helpers/api_client.dart';
+import 'package:smartklinik/model/pegawai.dart';
 
-class LoginService{
-  Future<bool> login(String username, String password) async{
+class LoginService {
+  Future<bool> login(String email, String password) async {
+    final Response response = await ApiClient().get('pegawai/?key=123');
+    print(response.data);
+
+    final List data = response.data['data'] as List;
+    List<Pegawai> result = data.map((json) => Pegawai.fromJson(json)).toList();
+
     bool isLogin = false;
-    if (username == 'admin' && password == 'admin'){
-      await UserInfo().setToken('admin');
-      await UserInfo().setUserID("1");
-      await UserInfo().setUserName('admin');
-      isLogin = true;
+
+    for (Pegawai pegawai in result) {
+      if (pegawai.email == email && pegawai.password == password) {
+        isLogin = true;
+        await UserInfo().setToken(pegawai.nama);
+        await UserInfo().setUserID(pegawai.nip);
+        await UserInfo().setUserName(pegawai.nama);
+        await UserInfo().setEmail(pegawai.email);
+        break; // Exit the loop once a match is found
+      }
     }
+
     return isLogin;
   }
 }
